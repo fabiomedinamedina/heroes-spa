d3171ac8e1145a8cb85c3bf1b7ce325dafd528e8
Fabio Medina <disenofabiomedina@gmail.com>
2023-04-12 16:41:20 -0500

    Update Single Hero Page, responsive. Add description data, Update CSS y componentes

--- src/heros/data/heros.js
+++ src/heros/data/heros.js
@@ -6,6 +6,7 @@ export const heroes = [
     alter_ego: "Bruce Wayne",
     first_appearance: "Detective Comics #27",
     characters: "Bruce Wayne",
+    description: "Batman (conocido inicialmente como Bat-Man y en español como el Hombre Murciélago) es un personaje de cómic creado por los estadounidenses Bob Kane y Bill Finger,13​ y propiedad de DC Comics. Apareció por primera vez en la historia titulada «El caso del sindicato químico» de la revista Detective Comics N.º 27, lanzada por la editorial National Publications el 30 de marzo de 1939. La identidad secreta de Batman es Bruce Wayne (Bruno Díaz en algunos países de habla hispana),14​15​16​ un multimillonario magnate empresarial y filántropo dueño de Empresas Wayne en Gotham City. Después de presenciar el asesinato de sus padres, el Dr. Thomas Wayne y Martha Wayne en un violento y fallido asalto cuando era niño, juró venganza contra los criminales, un juramento moderado por el sentido de la justicia."
   },
   {
     id: "dc-superman",
@@ -14,6 +15,7 @@ export const heroes = [
     alter_ego: "Kal-El",
     first_appearance: "Action Comics #1",
     characters: "Kal-El",
+    description: "Superman (en español: Superhombre) es un superhéroe ficticio que apareció por primera vez en los cómics estadounidenses publicados por DC Comics.1​2​3​4​El personaje fue creado por el escritor estadounidense Jerry Siegel y el artista canadiense Joe Shuster en 1933 cuando ambos se encontraban viviendo en Cleveland, Ohio. Sus creadores lo vendieron a Detective Comics, Inc. en 1938 por 130 dólares5​ y la primera aventura del personaje fue publicada en Action Comics #1 (junio de 1938), para luego aparecer en varios seriales de radio, programas de televisión, películas, tiras periódicas y videojuegos. Con el éxito de sus aventuras, este personaje definió al superhéroe y estableció su primacía dentro del cómic estadounidense.1​ Su apariencia del personaje es distintiva y se volvió icónica: un traje azul y rojo, con una capa y un emblema con una letra “S” estilizada en su pecho,6​7​8​ el cual se ha convertido en un símbolo del personaje."
   },
   {
     id: "dc-flash",
@@ -22,6 +24,7 @@ export const heroes = [
     alter_ego: "Jay Garrick",
     first_appearance: "Flash Comics #1",
     characters: "Jay Garrick, Barry Allen, Wally West, Bart Allen",
+    description: "Flash (conocido también como The Flash y traducido en español: Destello) es el nombre de varios superhéroes ficticios que aparecen en los cómics estadounidenses publicados por DC Comics. Creado por el escritor Gardner Fox y el artista Harry Lampert, el 'Flash' original apareció por primera vez en Flash Comics #1 (fecha de portada de enero de 1940 / mes de noviembre de 1939).1​ Apodado el 'Corredor Escarlata', todas las encarnaciones del Flash poseen 'súper velocidad', que incluye la capacidad de correr, moverse y pensar extremadamente rápido, también puede atravesar la materia sólida, usar reflejos sobrehumanos y aparentemente violar ciertas leyes de la física, como superar la velocidad de la luz."
   },
   {
     id: "dc-green",
@@ -31,6 +34,8 @@ export const heroes = [
     first_appearance: "All-American Comics #16",
     characters:
       "Alan Scott, Hal Jordan, Guy Gardner, John Stewart, Kyle Raynor, Jade, Sinestro, Simon Baz",
+    description:
+      "Linterna Verde (en inglés: Green Lantern) es el alias de varios superhéroes de la ficción del Universo DC, de la editorial DC Comics, los cuales se caracterizan por portar un anillo de poder y tener la capacidad de crear manifestaciones de luz sólida con dichos anillos. El primer Linterna Verde (Alan Scott) hizo su debut en julio de 1940 en el All-American Comics #16 (portada de Sheldon Moldoff) durante la Edad de Oro del Cómic estadounidense.1​ Su creación se la debemos al guionista Bill Finger y al dibujante Martín Nodell."
   },
   {
     id: "dc-arrow",
@@ -39,6 +44,7 @@ export const heroes = [
     alter_ego: "Oliver Queen",
     first_appearance: "More Fun Comics #73",
     characters: "Oliver Queen",
+    description: 'Green Arrow (en español: Flecha Verde) es un superhéroe que aparece en los cómics publicados por DC Comics. Creado por Mort Weisinger y diseñado por George Papp, apareció por primera vez en More Fun Comics #73 en noviembre de 1941. Su nombre real es Oliver Jonas Queen, un empresario adinerado y propietario de Industrias Queen que también es una celebridad muy conocida en Star City. Utiliza esta posición para ocultar el hecho de que él es la Flecha.1​A veces se muestra vestido como el personaje de Robin Hood, Green Arrow es un arquero que usa sus habilidades para combatir el crimen en sus ciudades natales de Star City y Seattle, así como junto a sus compañeros superhéroes como miembro de la Liga de la Justicia. '
   },
   {
     id: "dc-wonder",
@@ -47,6 +53,7 @@ export const heroes = [
     alter_ego: "Princess Diana",
     first_appearance: "All Star Comics #8",
     characters: "Princess Diana",
+    description: "La Mujer Maravilla (en inglés: Wonder Woman) es una superheroína ficticia creada por William Moulton Marston para la editorial DC Comics. Es una princesa guerrera de las Amazonas, pueblo ficticio basado en el de las amazonas de la mitología griega. En su tierra natal es conocida como la princesa Diana de Temiscira pero fuera de esta utiliza la identidad secreta de Diana Prince. Está dotada de una amplia gama de poderes superhumanos y habilidades de combate de batalla superiores, gracias a sus dones obtenidos de los dioses y su amplio entrenamiento. Ella posee un gran arsenal de armas, incluyendo entre las principales el Lazo de la Verdad, un par de brazaletes mágicos indestructibles."
   },
   {
     id: "dc-martian",
@@ -55,6 +62,7 @@ export const heroes = [
     alter_ego: "J'onn J'onzz",
     first_appearance: "Detective Comics #225",
     characters: "Martian Manhunter",
+    description: "El Detective Marciano (J'onn J'onzz) (en inglés: Martian Manhunter) es un superhéroe que aparece en los cómics estadounidenses publicados por DC Comics. Creado por el escritor Joseph Samachson y diseñado por el artista Joe Certa, el personaje apareció por primera vez en la historia 'The Manhunter from Mars' en Detective Comics # 225 (noviembre de 1955). Martian Manhunter es uno de los siete miembros originales de la Liga de la Justicia de América y uno de los seres más poderosos del Universo DC."
   },
   {
     id: "dc-robin",
@@ -63,6 +71,7 @@ export const heroes = [
     alter_ego: "Dick Grayson",
     first_appearance: "Detective Comics #38",
     characters: "Dick Grayson",
+    description: "Robin es el alias de varios superhéroes ficticios que aparecen en los cómics estadounidenses publicados por DC Comics. El personaje fue creado originalmente por Bob Kane, Bill Finger y Jerry Robinson, para servir como contraparte menor del superhéroe Batman. La primera encarnación del personaje, Dick Grayson, debutó en Detective Comics # 38 (abril de 1940). Robin hizo apariciones regulares en cómics relacionados con Batman y otras publicaciones de DC Comics desde 1940 hasta principios de la década de 1980 hasta que el personaje dejó de lado la identidad de Robin y se convirtió en el superhéroe independiente Nightwing. El equipo de Batman y Robin se conoce comúnmente como los Caped Crusaders o Dynamic Duo."
   },
   {
     id: "dc-blue",
@@ -71,6 +80,7 @@ export const heroes = [
     alter_ego: "Dan Garret",
     first_appearance: "Mystery Men Comics #1",
     characters: "Dan Garret, Ted Kord, Jaime Reyes",
+    description: "Escarabajo Azul (inglés: Blue Beetle) es el nombre de tres superhéroes ficticios que aparecen en varios cómics estadounidenses publicados por una variedad de compañías desde 1939. La más reciente de las compañías en poseer los derechos del Blue Beetle es DC Comics, que compró los derechos del personaje en 1983, utilizando el nombre de tres personajes distintos a lo largo de los años. El Blue Beetle original fue creado por Fox Comics y luego propiedad de Charlton Comics. El primer Beetle fue Dan Garret (más tarde deletreado Dan Garrett), quien inicialmente obtuvo superpoderes de una vitamina especial, que luego se cambió para obtener poderes de un 'escarabajo sagrado'. El Blue Beetle original apareció no solo en su propio cómic, sino también en una serie de radio semanal."
   },
   {
     id: "dc-black",
@@ -79,6 +89,7 @@ export const heroes = [
     alter_ego: "Dinah Drake",
     first_appearance: "Flash Comics #86",
     characters: "Dinah Drake, Dinah Lance",
+    description: "Canario Negro (en inglés, Black Canary) es una superheroína ficticia que aparece en los cómics estadounidenses publicados por DC Comics. Creado por el equipo de escritores y artistas de Robert Kanigher y Carmine Infantino, el personaje debutó en Flash Comics # 86 (agosto de 1947). Una de los primeros super-heroínas de DC, Canario Negro ha aparecido en muchas de insignia de la compañía Team-Up títulos como Sociedad de la Justicia de América y la Liga de la Justicia de América. Desde finales de la década de 1960, el personaje ha sido emparejado con el superhéroe arquero Flecha Verde, profesional y románticamente."
   },
   {
     id: "marvel-spider",
@@ -87,6 +98,7 @@ export const heroes = [
     alter_ego: "Peter Parker",
     first_appearance: "Amazing Fantasy #15",
     characters: "Peter Parker",
+    description: "Spider-Man, traducido en ocasiones como el Hombre Araña,11​12​ es un personaje creado por los estadounidenses Stan Lee y Steve Ditko,13​14​ e introducido en el cómic Amazing Fantasy n.° 15, publicado por Marvel Comics en agosto de 1962.15​ Se trata de un superhéroe que emplea sus habilidades sobrehumanas, reminiscentes de una araña, para combatir a otros supervillanos que persiguen fines siniestros. Su creación se remonta a principios de la década de 1960 como respuesta al creciente interés del público adolescente en los cómics y el éxito de Los 4 Fantásticos.16​ Tras su primera aparición en Amazing Fantasy,17​ Marvel decidió producir una serie individual titulada The Amazing Spider-Man, cuyo ejemplar inicial salió a la venta en marzo de 1963."
   },
   {
     id: "marvel-captain",
@@ -95,6 +107,7 @@ export const heroes = [
     alter_ego: "Steve Rogers",
     first_appearance: "Captain America Comics #1",
     characters: "Steve Rogers",
+    description: "El Capitán América, cuyo nombre real es Steven 'Steve' Grant Rogers, fue un superhéroe ficticio que aparece en los cómics estadounidenses publicados por Marvel Comics. Creado por los historietistas Joe Simon y Jack Kirby, el personaje apareció por primera vez en Captain America Comics #1 (marzo de 1941) de Timely Comics, predecesor de Marvel Comics. El Capitán América fue diseñado como un supersoldado patriota que luchaba frecuentemente contra las potencias del Eje en la Segunda Guerra Mundial, y fue el personaje más popular de Timely Comics durante el período de guerra."
   },
   {
     id: "marvel-iron",
@@ -103,6 +116,7 @@ export const heroes = [
     alter_ego: "Tony Stark",
     first_appearance: "Tales of Suspense #39",
     characters: "Tony Stark",
+    description: "Iron Man (conocido en español como el Hombre de Hierro) es un superhéroe que aparece en los cómics estadounidenses publicados por Marvel Comics nacido en Sofía, Bulgaria. El personaje fue cocreado por el escritor y editor Stan Lee y el guionista Larry Lieber. Los artistas Don Heck y Jack Kirby fueron los encargados de su diseño. Anthony Edward 'Tony' Stark es un personaje ficticio que da vida a este personaje, un multimillonario magnate empresarial y filántropo estadounidense, playboy e ingenioso científico, que sufrió una grave lesión en el pecho durante un secuestro en el Medio Oriente."
   },
   {
     id: "marvel-thor",
@@ -111,6 +125,7 @@ export const heroes = [
     alter_ego: "Thor Odinson",
     first_appearance: "Journey into Myster #83",
     characters: "Thor Odinson",
+    description: "Thor es un superhéroe ficticio que aparece en los cómics estadounidenses publicados por Marvel Comics. Es el dios del trueno asgardiano basado en la deidad homónima, del pueblo ficticio inspirado en el mundo de Asgard de la mitología nórdica. En su tierra natal es conocido como Thor Odinson pero fuera de esta utiliza la identidad secreta de Donald Blake.1​ Es poseedor del martillo encantado, Mjolnir, que le otorga capacidad de volar y manipular el clima entre sus otros atributos sobrehumanos, además de concentrar su poder."
   },
   {
     id: "marvel-hulk",
@@ -119,6 +134,7 @@ export const heroes = [
     alter_ego: "Bruce Banner",
     first_appearance: "The Incredible Hulk #1",
     characters: "Bruce Banner",
+    description: "Hulk (llamado Hulk o El Hombre Increíble en muchas de las traducciones al español) es un personaje ficticio, un superhéroe que aparece en los cómics estadounidenses publicados por la editorial Marvel Comics, es considerado el personaje más fuerte de Marvel Comics. El personaje fue creado por los escritores Stan Lee y Jack Kirby siendo su primera aparición en The Incredible Hulk #1 publicado en mayo de 1962. En sus apariciones de cómic, el personaje es a la vez Hulk, un ser humanoide enorme de piel verde, corpulento y musculoso que posee una gran fortaleza física, y su alter ego el Dr. Robert Bruce Banner"
   },
   {
     id: "marvel-wolverine",
@@ -127,6 +143,7 @@ export const heroes = [
     alter_ego: "James Howlett",
     first_appearance: "The Incredible Hulk #180",
     characters: "James Howlett",
+    description: "Wolverine, cuyo nombre de nacimiento es James Howlett (también conocido como James Logan o simplemente Logan)3​ es un superhéroe ficticio que aparece en los cómics publicados por Marvel Comics, principalmente en asociación con los X-Men. Es un mutante que posee sentidos afinados a los animales, capacidades físicas mejoradas, poderosa capacidad de regeneración conocida como un factor de curación, y tres garras retráctiles en cada mano. Wolverine ha sido representado de diversas formas como miembro de los X-Men, Alpha Flight, Fuerza-X y Los Vengadores."
   },
   {
     id: "marvel-daredevil",
@@ -135,6 +152,7 @@ export const heroes = [
     alter_ego: "Matthew Michael Murdock",
     first_appearance: "Daredevil #1",
     characters: "Matthew Michael Murdock",
+    description: "Daredevil (también llamado Dan Defensor, Diablo Defensor o Diabólico en muchas de las traducciones al español), alter ego de Matthew Michael 'Matt' Murdock, es un superhéroe ficticio que aparece en los cómics estadounidenses publicados por Marvel Comics. Daredevil fue creado por el escritor y editor Stan Lee y el artista Bill Everett, con una cantidad no especificada de aportes de Jack Kirby. El personaje apareció por primera vez en Daredevil #1 (abril de 1964) en la Edad de Plata de los cómics."
   },
   {
     id: "marvel-hawkeye",
@@ -143,6 +161,7 @@ export const heroes = [
     alter_ego: "Clinton Francis Barton",
     first_appearance: "Tales of Suspense #57",
     characters: "Clinton Francis Barton",
+    description: "Hawkeye (Clinton Francis «Clint» Barton), es un superhéroe de ficción que aparece en los cómics estadounidenses publicados por Marvel Comics. Creado por el escritor Stan Lee y el artista Don Heck, el personaje apareció por primera vez como un villano en Tales of Suspense # 57 (septiembre de 1964) y más tarde se unió a Los Vengadores en The Avengers # 16 (mayo de 1965). Él ha sido un miembro prominente del equipo desde entonces. También ocupó el puesto # 44 en la lista de los 100 mejores héroes de cómics de IGN"
   },
   {
     id: "marvel-cyclops",
@@ -151,6 +170,7 @@ export const heroes = [
     alter_ego: "Scott Summers",
     first_appearance: "X-Men #1",
     characters: "Scott Summers",
+    description: "Cíclope (Scott Summers) es un personaje ficticio que aparece en los cómics estadounidenses publicados por Marvel Comics y es miembro fundador de los X-Men. Creado por el escritor Stan Lee y el artista Jack Kirby, el personaje apareció por primera vez en el cómic The X-Men de 1963, en la Edad de Plata de los cómics. Cíclope es miembro de una subespecie de humanos conocidos como mutantes, que nacen con habilidades sobrehumanas. Cíclope emite poderosos rayos de energía de sus ojos y solo puede controlar los rayos con la ayuda de gafas especiales que debe usar en todo momento. "
   },
   {
     id: "marvel-silver",
@@ -159,5 +179,6 @@ export const heroes = [
     alter_ego: "Norrin Radd",
     first_appearance: "The Fantastic Four #48",
     characters: "Norrin Radd",
+    description: "Silver Surfer (también conocido en Hispanoamérica como Surfista de Plata, Surfista Plateado o Deslizador de plata, y en España como Estela Plateada) es un personaje ficticio de cómic del Universo Marvel creado por Jack Kirby. Su primera aparición fue en Los 4 Fantásticos n.° 48 (marzo de 1966). El personaje también aparece en adaptaciones al cine, la televisión y los videojuegos. El Silver Surfer es un extraterrestre humanoide con piel metálica que puede viajar a través del espacio con la ayuda de su nave, similar a una tabla de surf. Originalmente un joven astrónomo de nombre Norrin Radd en el planeta Zenn-La, salvó a su planeta de Galactus, el devorador de planetas, sirviendo como su heraldo."
   },
 ];
--- src/heros/pages/HeroPage.jsx
+++ src/heros/pages/HeroPage.jsx
@@ -10,7 +10,8 @@ export const HeroPage = () => {
 
   if( !hero ) return <Navigate to="/marvel"/>
   
-  const { superhero, publisher , alter_ego, first_appearance, characters } = hero;
+  const { superhero, publisher , alter_ego, first_appearance, characters, description } = hero;
+  const publisherSlug = publisher.replace(/\s+/g, '-').toLowerCase();
 
   const onNavigateBack = () => {
     
@@ -25,41 +26,74 @@ export const HeroPage = () => {
 
 
   return (
-    <div className='row mt-5'>
-      <div className="col-4">
-        <img
-          src={`/assets/heroes/${ id }.jpg`}
-          alt={ superhero }
-          className="img-fluid rounded shadow"
-        />
-      </div>
-      <div className="col-8 mt-3 ps-5">
-        <h1 className='d-flex align-items-center justify-content-between'>
-          { superhero }
-          <span className="badge rounded-pill text-bg-dark fs-6">
-            <small>{ publisher }</small>
-          </span>
-        </h1>
-        <hr />
-        <ul className="list-group list-group-flush mb-3">
-          <li className="list-group-item">
-            <strong>Alter Ego:</strong> { alter_ego }
-          </li>
-          <li className="list-group-item">
-            <strong>First Appearance:</strong> { first_appearance }
-          </li>
-          <li className="list-group-item">
-            <strong>Characters:</strong> { characters }
-          </li>
-        </ul>
-
-        <button
-          className='btn btn-outline-success rounded-pill btn-sm ps-4 pe-4'
-          onClick={ onNavigateBack }
-        >
-          Back to publisher
-        </button>
+    <div className="container">
+      <div className="card card-hero card-hero-single rounded-4 border-0">
+      <div className="row g-0 align-items-center">
+        <div className="col-md-4">
+          <img
+            src={`/assets/heroes/${ id }.jpg`}
+            alt={ superhero }
+            className="img-fluid rounded-start-2"
+          />
+        </div>
+        <div className="col-md-8">
+          <div className="card-body">
+            <h1 className='d-flex flex-column flex-lg-row align-items-start align-items-lg-center justify-content-between fw-bold mb-0'>
+              <span className='order-1 order-lg-0'>{ superhero }</span>
+              <span className={ `order-0 order-lg-1 mb-3 mb-lg-0 badge fs-5 rounded-pill badge-${ publisherSlug }` }>
+                <small>{ publisher }</small>
+              </span>
+            </h1>
+            <h2 className='fs-4 mb-3' >{ alter_ego }</h2>
+            <p className="card-text">{ description }</p>
+            <p className="card-text mb-0 text-color-secondary"><strong>Characters:</strong> { characters }</p>
+            <p className="card-text mb-0 text-color-secondary"><strong>First Appearance:</strong> { first_appearance }</p>
+            <button
+              className='btn btn-login rounded-pill ps-4 pe-4 mt-4'
+              onClick={ onNavigateBack }
+            >
+              Back to publisher
+            </button>
+          </div>
+        </div>
       </div>
     </div>
+    </div>
+    // <div className='row mt-5'>
+    //   <div className="col-4">
+    //     <img
+    //       src={`/assets/heroes/${ id }.jpg`}
+    //       alt={ superhero }
+    //       className="img-fluid rounded shadow"
+    //     />
+    //   </div>
+    //   <div className="col-8 mt-3 ps-5">
+    //     <h1 className='d-flex align-items-center justify-content-between'>
+    //       { superhero }
+    //       <span className="badge rounded-pill text-bg-dark fs-6">
+    //         <small>{ publisher }</small>
+    //       </span>
+    //     </h1>
+    //     <hr />
+    //     <ul className="list-group list-group-flush mb-3">
+    //       <li className="list-group-item">
+    //         <strong>Alter Ego:</strong> { alter_ego }
+    //       </li>
+    //       <li className="list-group-item">
+    //         <strong>First Appearance:</strong> { first_appearance }
+    //       </li>
+    //       <li className="list-group-item">
+    //         <strong>Characters:</strong> { characters }
+    //       </li>
+    //     </ul>
+
+    //     <button
+    //       className='btn btn-outline-success rounded-pill btn-sm ps-4 pe-4'
+    //       onClick={ onNavigateBack }
+    //     >
+    //       Back to publisher
+    //     </button>
+    //   </div>
+    // </div>
   );
 };
